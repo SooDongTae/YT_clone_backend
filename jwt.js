c0d5e4f46f7d6bdbe2b50be9f97b64ef026cc2ed
DoHi0512 <donghuni512@gmail.com>
2022-09-28 11:24:49 +0900

    feat : 로그인, 회원가입

--- jwt.js
+++ jwt.js
@@ -1,36 +1,47 @@
-require("dotenv").config();
-
 const http = require("http");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const { Router, response } = require("express");
-
+require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 const PORT = 8080;
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const mysql = require("mysql");
+const conn = {
+  host: "localhost",
+  port: "3306",
+  user: "sdt",
+  password: "1234",
+  database: "yt",
+};
+let connection = mysql.createConnection(conn);
+
+const profiles = [
+  "http://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2020/03/27/5561b209-4809-4c6e-9f8b-33d0e7792de8.jpg",
+  "https://photo.newsen.com/mphoto/2022/06/24/202206241807463510_1.jpg",
+  "https://cdn.spotvnews.co.kr/news/photo/202112/458893_583105_1601.jpg",
+];
+
 app.post("/register", function (req, res, next) {
-  const mysql = require("mysql");
-  const conn = {
-    host: "localhost",
-    port: "3306",
-    user: "sdt",
-    password: "1234",
-    database: "yt",
-  };
   const username = req.body.username;
   const nickname = req.body.nickname;
   const password = req.body.password;
-  const profile = req.body.profile;
   let accessToken = generateAccessToken(username);
   let refreshToken = generateRefreshToken(username);
-  let connection = mysql.createConnection(conn);
   connection.connect();
-  let sql = `insert into user(username,nickname,password,profile,access_token,refresh_token) values ('${username}','${nickname}','${password}','${profile}','${accessToken}','${refreshToken}');`;
+  const random = Math.floor(Math.random() * 3);
+
+  let sql = `insert into user(username,nickname,password,profile,access_token,refresh_token) values ('${username}','${nickname}','${password}','${profiles[random]}','${accessToken}','${refreshToken}');`;
   connection.query(sql, function (err, result, fields) {
     if (err) {
       console.log(err);
@@ -39,14 +50,14 @@ app.post("/register", function (req, res, next) {
         message: "Id is already used.",
       });
     }
+
+    console.log(result);
     return res.status(200).json({
+      profile: profiles[random],
       code: 200,
-      token: accessToken,
       message: "OK",
     });
-    console.log(result);
   });
-
 
   // console.log(accessToken);
   // console.log(refreshToken);
@@ -56,12 +67,8 @@ app.post("/register", function (req, res, next) {
   //   console.log(results);
   // });
   // connection.end();
-  
 });
 
-const login = (id, pw) => {
-  return id;
-};
 const generateAccessToken = (id) => {
   return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "15m",
@@ -74,16 +81,51 @@ const generateRefreshToken = (id) => {
   });
 };
 
-app.post("/login", (req, res) => {
-  let id = req.body.name;
-  let pw = req.body.password;
-  let user = login(id, pw);
-  let accessToken = generateAccessToken(user);
-  let refreshToken = generateRefreshToken(user);
-
-  res.json({ accessToken, refreshToken });
+app.post("/upload", (req, res) => {
+  connection.connect();
+  const title = req.body.title;
+  const content = req.body.text;
+  const tags = req.body.tags;
+  for (let i = 0; i < tegs.length; i++) {
+    let sql = `select cate_id from cate where cate_title = ${tags[i]};`;
+    connection.query(sql, function (err, result) {
+      if (result.length === 0) {
+        sql = `insert into cate(cate_title) values(${tags[i]});`;
+        connection.query(sql, function (err, result) {
+          if (err) {
+            console.log("insert faild");
+          } else {
+            console.log("insert success");
+          }
+        });
+      } else {
+        console.log(result);
+      }
+    });
+  }
 });
 
+app.post("/login", (req, res) => {
+  connection.connect();
+  let username = req.body.username;
+  let password = req.body.password;
+  let token;
+  let sql = `select access_token from user where username = '${username}' and password = '${password}';`;
+  connection.query(sql, function (err, result, fields) {
+    if (result.length === 0) {
+      return res.status(400).json({
+        code: 400,
+        message: "ID or PASSWORD is wrong.",
+      });
+    }
+    token = result[0].access_token;
+    return res.status(200).json({
+      code: 200,
+      token: token,
+      message: "OK",
+    });
+  });
+});
 const authenticateAccessToken = (req, res, next) => {
   let authHeader = req.headers["authorization"];
   let token = authHeader && authHeader.split(" ")[1];
